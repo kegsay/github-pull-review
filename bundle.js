e3122ae902951eda6dac8cdb95f90c53489c2949
Kegan Dougal <kegsay@gmail.com>
2015-09-19 18:27:33 +0100

    Update with owner/repo and pr num

--- bundle.js
+++ bundle.js
@@ -8,30 +8,56 @@ React.render(
 );
 
 },{"./components/main-page":3}],2:[function(require,module,exports){
+var ID_ACCESS_TOKEN = "info_access_token";
+var ID_OWNER_REPO = "info_owner_repo";
+var ID_PR_NUM = "info_pr_num";
+
+var IDS = [
+    ID_ACCESS_TOKEN, ID_PR_NUM, ID_OWNER_REPO
+];
+
 module.exports = React.createClass({displayName: 'InfoGetter',
 
     getInitialState: function() {
-        return {
-            token: localStorage.getItem("access_token")
-        };
+        var initial = {};
+        IDS.forEach(function(id) {
+            initial[id] = localStorage.getItem(id);
+        });
+        return initial;
     },
 
     handleChange: function(event) {
-        this.setState({
-            token: event.target.value
-        });
-        localStorage.setItem("access_token", event.target.value);
+        var changeState = {};
+        changeState[event.target.id] = event.target.value;
+        this.setState(changeState);
+        localStorage.setItem(event.target.id, event.target.value);
     },
 
     render: function() {
         var ele = React.createElement.bind(React);
         var token = this.state.token;
-        return ele("div", null,
+        return ele("div", null, [
             ele("input", {
                 type: "text", placeholder: "access_token",
-                value: token, onChange: this.handleChange
-            })
-        );
+                id: ID_ACCESS_TOKEN,
+                value: this.state[ID_ACCESS_TOKEN],
+                onChange: this.handleChange
+            }),
+            ele("div", null, [
+                ele("input", {
+                    type: "text", placeholder: "owner/repo",
+                    id: ID_OWNER_REPO,
+                    value: this.state[ID_OWNER_REPO],
+                    onChange: this.handleChange
+                }),
+                ele("input", {
+                    type: "text", placeholder: "Pull Request #",
+                    id: ID_PR_NUM,
+                    value: this.state[ID_PR_NUM],
+                    onChange: this.handleChange
+                })
+            ])
+        ]);
     }
 });
 
