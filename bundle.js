3442ef77c7f38d7b8aaa874b379beb0fdcdac8e2
Kegan Dougal <kegsay@gmail.com>
2015-09-20 11:26:06 +0100

    Update to d20e26d

--- bundle.js
+++ bundle.js
@@ -4,72 +4,568 @@ var MainPage = require("./components/main-page");
 
 React.render(
     React.createElement(MainPage),
-    document.getElementById('container')
+    document.getElementById("container")
 );
 
 },{"./components/main-page":3}],2:[function(require,module,exports){
-var ID_ACCESS_TOKEN = "info_access_token";
-var ID_OWNER_REPO = "info_owner_repo";
-var ID_PR_NUM = "info_pr_num";
-
-var IDS = [
-    ID_ACCESS_TOKEN, ID_PR_NUM, ID_OWNER_REPO
-];
+var dispatcher = require("../logic/dispatcher");
+var SessionStore = require("../logic/session-store");
+var KEY_TOKEN = "info_access_token";
+var KEY_REPO = "info_repo_id";
+var KEY_REQ = "info_req_id";
 
 module.exports = React.createClass({displayName: 'InfoGetter',
 
+    getDefaultProps: function() {
+        return {
+            store: new SessionStore(localStorage)
+        };
+    },
+
     getInitialState: function() {
-        var initial = {};
-        IDS.forEach(function(id) {
-            initial[id] = localStorage.getItem(id);
-        });
-        return initial;
+        var initialState = {};
+        initialState[KEY_TOKEN] = this.props.store.getAccessToken();
+        initialState[KEY_REPO] = this.props.store.getRepositoryId();
+        initialState[KEY_REQ] = this.props.store.getRequestId();
+        return initialState;
     },
 
     handleChange: function(event) {
+        var newVal = event.target.value;
         var changeState = {};
-        changeState[event.target.id] = event.target.value;
+        changeState[event.target.id] = newVal;
         this.setState(changeState);
-        localStorage.setItem(event.target.id, event.target.value);
+        switch (event.target.id) {
+            case KEY_REPO:
+                this.props.store.setRepositoryId(newVal);
+                break;
+            case KEY_REQ:
+                this.props.store.setRequestId(newVal);
+                break;
+            case KEY_TOKEN:
+                this.props.store.setAccessToken(newVal);
+                break;
+        }
+    },
+
+    onView: function() {
+        dispatcher.dispatch({
+            action: "view_pr",
+            data: {
+                repo_id: this.state[KEY_REPO],
+                request_id: this.state[KEY_REQ]
+            }
+        });
     },
 
     render: function() {
-        var ele = React.createElement.bind(React);
-        var token = this.state.token;
-        return ele("div", null, [
-            ele("input", {
-                type: "text", placeholder: "access_token",
-                id: ID_ACCESS_TOKEN,
-                value: this.state[ID_ACCESS_TOKEN],
-                onChange: this.handleChange
-            }),
-            ele("div", null, [
-                ele("input", {
-                    type: "text", placeholder: "owner/repo",
-                    id: ID_OWNER_REPO,
-                    value: this.state[ID_OWNER_REPO],
-                    onChange: this.handleChange
-                }),
-                ele("input", {
-                    type: "text", placeholder: "Pull Request #",
-                    id: ID_PR_NUM,
-                    value: this.state[ID_PR_NUM],
-                    onChange: this.handleChange
-                })
-            ])
-        ]);
-    }
-});
+        return (
+            React.createElement("div", null, 
+                React.createElement("input", {type: "text", placeholder: "access_token", 
+                id: KEY_TOKEN, value: this.state[KEY_TOKEN], 
+                onChange: this.handleChange}), 
 
-},{}],3:[function(require,module,exports){
-var InfoGetter = require("./info-getter");
+                React.createElement("div", null, 
+                    React.createElement("input", {type: "text", placeholder: "owner/repo", 
+                    id: KEY_REPO, value: this.state[KEY_REPO], 
+                    onChange: this.handleChange}), 
 
-module.exports = React.createClass({displayName: 'MainPage',
-    render: function() {
-        return React.createElement("div", null,
-            React.createElement(InfoGetter)
+                    React.createElement("input", {type: "text", placeholder: "Pull Request #", 
+                    id: KEY_REQ, value: this.state[KEY_REQ], 
+                    onChange: this.handleChange}), 
+
+                    React.createElement("button", {onClick: this.onView}, "View")
+                )
+            )
         );
     }
 });
 
-},{"./info-getter":2}]},{},[1]);
+},{"../logic/dispatcher":5,"../logic/session-store":6}],3:[function(require,module,exports){
+var InfoGetter = require("./info-getter");
+var PullRequestOverview = require("./pull-request-overview");
+
+module.exports = React.createClass({displayName: 'MainPage',
+
+    render: function() {
+        return (
+            React.createElement("div", null, 
+                React.createElement(InfoGetter, null), 
+                React.createElement(PullRequestOverview, null)
+            )
+        );
+    }
+});
+
+},{"./info-getter":2,"./pull-request-overview":4}],4:[function(require,module,exports){
+var dispatcher = require("../logic/dispatcher");
+
+module.exports = React.createClass({displayName: 'PullRequestOverview',
+
+    componentDidMount: function() {
+        this._dispatcherRef = dispatcher.register(this.onAction);
+    },
+
+    componentWillUnmount: function() {
+        dispatcher.unregister(this._dispatcherRef);
+    },
+
+    onAction: function(payload) {
+        switch (payload.action) {
+            case "view_pr":
+                this.setState({
+                    view_pr: payload.data
+                });
+                break;
+        }
+    },
+
+    getInitialState: function() {
+        return {
+            view_pr: {}
+        };
+    },
+
+    render: function() {
+        return (
+            React.createElement("div", null, 
+                "Overview ", this.state.view_pr.request_id
+            )
+        );
+    }
+});
+
+},{"../logic/dispatcher":5}],5:[function(require,module,exports){
+"use strict";
+var Dispatcher = require("flux").Dispatcher;
+
+module.exports = new Dispatcher();
+
+},{"flux":8}],6:[function(require,module,exports){
+"use strict";
+var ID_ACCESS_TOKEN = "SessionStore_access_token";
+var ID_REPO_ID = "SessionStore_repo_id";
+var ID_REQUEST_ID = "SessionStore_request_id";
+
+function SessionStore(storageInterface) {
+    this.store = storageInterface;
+}
+
+SessionStore.prototype.getAccessToken = function() {
+    return this.store.getItem(ID_ACCESS_TOKEN);
+};
+
+SessionStore.prototype.getRepositoryId = function() {
+    return this.store.getItem(ID_REPO_ID);
+};
+
+SessionStore.prototype.getRequestId = function() {
+    return this.store.getItem(ID_REQUEST_ID);
+};
+
+SessionStore.prototype.setAccessToken = function(token) {
+    this.store.setItem(ID_ACCESS_TOKEN, token);
+};
+
+SessionStore.prototype.setRepositoryId = function(repoId) {
+    this.store.setItem(ID_REPO_ID, repoId);
+};
+
+SessionStore.prototype.setRequestId = function(reqId) {
+    this.store.setItem(ID_REQUEST_ID, reqId);
+};
+
+module.exports = SessionStore;
+
+},{}],7:[function(require,module,exports){
+// shim for using process in browser
+
+var process = module.exports = {};
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+
+function cleanUpNextTick() {
+    draining = false;
+    if (currentQueue.length) {
+        queue = currentQueue.concat(queue);
+    } else {
+        queueIndex = -1;
+    }
+    if (queue.length) {
+        drainQueue();
+    }
+}
+
+function drainQueue() {
+    if (draining) {
+        return;
+    }
+    var timeout = setTimeout(cleanUpNextTick);
+    draining = true;
+
+    var len = queue.length;
+    while(len) {
+        currentQueue = queue;
+        queue = [];
+        while (++queueIndex < len) {
+            if (currentQueue) {
+                currentQueue[queueIndex].run();
+            }
+        }
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    clearTimeout(timeout);
+}
+
+process.nextTick = function (fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) {
+        for (var i = 1; i < arguments.length; i++) {
+            args[i - 1] = arguments[i];
+        }
+    }
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) {
+        setTimeout(drainQueue, 0);
+    }
+};
+
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function () {
+    this.fun.apply(null, this.array);
+};
+process.title = 'browser';
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
+
+function noop() {}
+
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+
+process.binding = function (name) {
+    throw new Error('process.binding is not supported');
+};
+
+process.cwd = function () { return '/' };
+process.chdir = function (dir) {
+    throw new Error('process.chdir is not supported');
+};
+process.umask = function() { return 0; };
+
+},{}],8:[function(require,module,exports){
+/**
+ * Copyright (c) 2014-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+module.exports.Dispatcher = require('./lib/Dispatcher');
+
+},{"./lib/Dispatcher":9}],9:[function(require,module,exports){
+(function (process){
+/**
+ * Copyright (c) 2014-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule Dispatcher
+ * 
+ * @preventMunge
+ */
+
+'use strict';
+
+exports.__esModule = true;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var invariant = require('fbjs/lib/invariant');
+
+var _prefix = 'ID_';
+
+/**
+ * Dispatcher is used to broadcast payloads to registered callbacks. This is
+ * different from generic pub-sub systems in two ways:
+ *
+ *   1) Callbacks are not subscribed to particular events. Every payload is
+ *      dispatched to every registered callback.
+ *   2) Callbacks can be deferred in whole or part until other callbacks have
+ *      been executed.
+ *
+ * For example, consider this hypothetical flight destination form, which
+ * selects a default city when a country is selected:
+ *
+ *   var flightDispatcher = new Dispatcher();
+ *
+ *   // Keeps track of which country is selected
+ *   var CountryStore = {country: null};
+ *
+ *   // Keeps track of which city is selected
+ *   var CityStore = {city: null};
+ *
+ *   // Keeps track of the base flight price of the selected city
+ *   var FlightPriceStore = {price: null}
+ *
+ * When a user changes the selected city, we dispatch the payload:
+ *
+ *   flightDispatcher.dispatch({
+ *     actionType: 'city-update',
+ *     selectedCity: 'paris'
+ *   });
+ *
+ * This payload is digested by `CityStore`:
+ *
+ *   flightDispatcher.register(function(payload) {
+ *     if (payload.actionType === 'city-update') {
+ *       CityStore.city = payload.selectedCity;
+ *     }
+ *   });
+ *
+ * When the user selects a country, we dispatch the payload:
+ *
+ *   flightDispatcher.dispatch({
+ *     actionType: 'country-update',
+ *     selectedCountry: 'australia'
+ *   });
+ *
+ * This payload is digested by both stores:
+ *
+ *   CountryStore.dispatchToken = flightDispatcher.register(function(payload) {
+ *     if (payload.actionType === 'country-update') {
+ *       CountryStore.country = payload.selectedCountry;
+ *     }
+ *   });
+ *
+ * When the callback to update `CountryStore` is registered, we save a reference
+ * to the returned token. Using this token with `waitFor()`, we can guarantee
+ * that `CountryStore` is updated before the callback that updates `CityStore`
+ * needs to query its data.
+ *
+ *   CityStore.dispatchToken = flightDispatcher.register(function(payload) {
+ *     if (payload.actionType === 'country-update') {
+ *       // `CountryStore.country` may not be updated.
+ *       flightDispatcher.waitFor([CountryStore.dispatchToken]);
+ *       // `CountryStore.country` is now guaranteed to be updated.
+ *
+ *       // Select the default city for the new country
+ *       CityStore.city = getDefaultCityForCountry(CountryStore.country);
+ *     }
+ *   });
+ *
+ * The usage of `waitFor()` can be chained, for example:
+ *
+ *   FlightPriceStore.dispatchToken =
+ *     flightDispatcher.register(function(payload) {
+ *       switch (payload.actionType) {
+ *         case 'country-update':
+ *         case 'city-update':
+ *           flightDispatcher.waitFor([CityStore.dispatchToken]);
+ *           FlightPriceStore.price =
+ *             getFlightPriceStore(CountryStore.country, CityStore.city);
+ *           break;
+ *     }
+ *   });
+ *
+ * The `country-update` payload will be guaranteed to invoke the stores'
+ * registered callbacks in order: `CountryStore`, `CityStore`, then
+ * `FlightPriceStore`.
+ */
+
+var Dispatcher = (function () {
+  function Dispatcher() {
+    _classCallCheck(this, Dispatcher);
+
+    this._callbacks = {};
+    this._isDispatching = false;
+    this._isHandled = {};
+    this._isPending = {};
+    this._lastID = 1;
+  }
+
+  /**
+   * Registers a callback to be invoked with every dispatched payload. Returns
+   * a token that can be used with `waitFor()`.
+   */
+
+  Dispatcher.prototype.register = function register(callback) {
+    var id = _prefix + this._lastID++;
+    this._callbacks[id] = callback;
+    return id;
+  };
+
+  /**
+   * Removes a callback based on its token.
+   */
+
+  Dispatcher.prototype.unregister = function unregister(id) {
+    !this._callbacks[id] ? process.env.NODE_ENV !== 'production' ? invariant(false, 'Dispatcher.unregister(...): `%s` does not map to a registered callback.', id) : invariant(false) : undefined;
+    delete this._callbacks[id];
+  };
+
+  /**
+   * Waits for the callbacks specified to be invoked before continuing execution
+   * of the current callback. This method should only be used by a callback in
+   * response to a dispatched payload.
+   */
+
+  Dispatcher.prototype.waitFor = function waitFor(ids) {
+    !this._isDispatching ? process.env.NODE_ENV !== 'production' ? invariant(false, 'Dispatcher.waitFor(...): Must be invoked while dispatching.') : invariant(false) : undefined;
+    for (var ii = 0; ii < ids.length; ii++) {
+      var id = ids[ii];
+      if (this._isPending[id]) {
+        !this._isHandled[id] ? process.env.NODE_ENV !== 'production' ? invariant(false, 'Dispatcher.waitFor(...): Circular dependency detected while ' + 'waiting for `%s`.', id) : invariant(false) : undefined;
+        continue;
+      }
+      !this._callbacks[id] ? process.env.NODE_ENV !== 'production' ? invariant(false, 'Dispatcher.waitFor(...): `%s` does not map to a registered callback.', id) : invariant(false) : undefined;
+      this._invokeCallback(id);
+    }
+  };
+
+  /**
+   * Dispatches a payload to all registered callbacks.
+   */
+
+  Dispatcher.prototype.dispatch = function dispatch(payload) {
+    !!this._isDispatching ? process.env.NODE_ENV !== 'production' ? invariant(false, 'Dispatch.dispatch(...): Cannot dispatch in the middle of a dispatch.') : invariant(false) : undefined;
+    this._startDispatching(payload);
+    try {
+      for (var id in this._callbacks) {
+        if (this._isPending[id]) {
+          continue;
+        }
+        this._invokeCallback(id);
+      }
+    } finally {
+      this._stopDispatching();
+    }
+  };
+
+  /**
+   * Is this Dispatcher currently dispatching.
+   */
+
+  Dispatcher.prototype.isDispatching = function isDispatching() {
+    return this._isDispatching;
+  };
+
+  /**
+   * Call the callback stored with the given id. Also do some internal
+   * bookkeeping.
+   *
+   * @internal
+   */
+
+  Dispatcher.prototype._invokeCallback = function _invokeCallback(id) {
+    this._isPending[id] = true;
+    this._callbacks[id](this._pendingPayload);
+    this._isHandled[id] = true;
+  };
+
+  /**
+   * Set up bookkeeping needed when dispatching.
+   *
+   * @internal
+   */
+
+  Dispatcher.prototype._startDispatching = function _startDispatching(payload) {
+    for (var id in this._callbacks) {
+      this._isPending[id] = false;
+      this._isHandled[id] = false;
+    }
+    this._pendingPayload = payload;
+    this._isDispatching = true;
+  };
+
+  /**
+   * Clear bookkeeping used for dispatching.
+   *
+   * @internal
+   */
+
+  Dispatcher.prototype._stopDispatching = function _stopDispatching() {
+    delete this._pendingPayload;
+    this._isDispatching = false;
+  };
+
+  return Dispatcher;
+})();
+
+module.exports = Dispatcher;
+}).call(this,require('_process'))
+},{"_process":7,"fbjs/lib/invariant":10}],10:[function(require,module,exports){
+(function (process){
+/**
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @providesModule invariant
+ */
+
+"use strict";
+
+/**
+ * Use invariant() to assert state which your program assumes to be true.
+ *
+ * Provide sprintf-style format (only %s is supported) and arguments
+ * to provide information about what broke and what you were
+ * expecting.
+ *
+ * The invariant message will be stripped in production, but the invariant
+ * will remain to ensure logic does not differ in production.
+ */
+
+var invariant = function (condition, format, a, b, c, d, e, f) {
+  if (process.env.NODE_ENV !== 'production') {
+    if (format === undefined) {
+      throw new Error('invariant requires an error message argument');
+    }
+  }
+
+  if (!condition) {
+    var error;
+    if (format === undefined) {
+      error = new Error('Minified exception occurred; use the non-minified dev environment ' + 'for the full error message and additional helpful warnings.');
+    } else {
+      var args = [a, b, c, d, e, f];
+      var argIndex = 0;
+      error = new Error('Invariant Violation: ' + format.replace(/%s/g, function () {
+        return args[argIndex++];
+      }));
+    }
+
+    error.framesToPop = 1; // we don't care about invariant's own frame
+    throw error;
+  }
+};
+
+module.exports = invariant;
+}).call(this,require('_process'))
+},{"_process":7}]},{},[1]);
