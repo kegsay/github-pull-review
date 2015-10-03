59662aff3506640511307e6011b9798566887729
Kegan Dougal <kegsay@gmail.com>
2015-10-03 21:48:23 +0100

    Update build file

--- build.js
+++ build.js
@@ -2,3 +2,5 @@ require("shelljs/global");
 
 cp("-f", "index.html", "build/index.html");
 cp("-f", "index.css", "build/index.css");
+cp("-f", "base2cmt.png", "build/base2cmt.png");
+cp("-f", "cmt2head.png", "build/cmt2head.png");
