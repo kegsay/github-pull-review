63f8293e389c46368d0ee578edea5e2663696991
Kegan Dougal <kegsay@gmail.com>
2015-10-11 18:45:41 +0100

    Bugfix release

--- bundle.js
+++ bundle.js
@@ -1933,6 +1933,28 @@ module.exports = new Dispatcher();
 var request = require("request");
 var Promise = require("bluebird");
 
+// Link:
+// <https://api.github.com/repositories/24918719/pulls/41/comments?page=2>; rel="next",
+// <https://api.github.com/repositories/24918719/pulls/41/comments?page=2>; rel="last"
+function parseLinkHeader(header) {
+    if (!header || header.length === 0) {
+        return {};
+    }
+    var parts = header.split(',');
+    var links = {};
+    // Parse each part into a named link
+    for (var i = 0; i < parts.length; i++) {
+        var section = parts[i].split(';');
+        if (section.length !== 2) {
+            continue; // something is weird with this link
+        }
+        var url = section[0].replace(/<(.*)>/, '$1').trim();
+        var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+        links[name] = url;
+    }
+    return links;
+}
+
 function GithubApi(endpoint, token) {
     this.url = endpoint;
     this.token = token;
@@ -1951,11 +1973,11 @@ GithubApi.prototype.getPullRequest = function(repo, pr) {
 };
 
 GithubApi.prototype.getLineComments = function(repo, pr) {
-    return this._get("/repos/" + repo + "/pulls/" + pr + "/comments");
+    return this._getAll(this.url + "/repos/" + repo + "/pulls/" + pr + "/comments");
 };
 
 GithubApi.prototype.getPullRequestComments = function(repo, pr) {
-    return this._get("/repos/" + repo + "/issues/" + pr + "/comments");
+    return this._getAll(this.url + "/repos/" + repo + "/issues/" + pr + "/comments");
 };
 
 GithubApi.prototype.getPullRequestCommits = function(repo, pr) {
@@ -1996,19 +2018,39 @@ GithubApi.prototype.postLineCommentResponse = function(repo, pr, text, commentId
     });
 };
 
+// resolve all Link: headers and cat them into the same response body array
+GithubApi.prototype._getAll = function(url) {
+    var self = this;
+    return this._getUrl(url).then(function(response) {
+        var links = parseLinkHeader(response.headers.link);
+        if (links.next) {
+            return self._getAll(links.next).then(function(nextResponse) {
+                // extend the array with this new response
+                response.body.push.apply(response.body, nextResponse.body);
+                return response;
+            });
+        }
+        return response;
+    });
+};
+
+GithubApi.prototype._getUrl = function(url) {
+    return this._req("GET", url);
+};
+
 GithubApi.prototype._get = function(path) {
-    return this._req("GET", path);
+    return this._req("GET", this.url + path);
 };
 
 GithubApi.prototype._post = function(path, data) {
-    return this._req("POST", path, data);
+    return this._req("POST", this.url + path, data);
 };
 
-GithubApi.prototype._req = function(method, path, data) {
+GithubApi.prototype._req = function(method, url, data) {
     var p = Promise.defer();
     var qs = this.token ? { access_token: this.token } : {};
     request({
-        uri: (this.url + path),
+        uri: url,
         method: method,
         json: true,
         withCredentials: false,
